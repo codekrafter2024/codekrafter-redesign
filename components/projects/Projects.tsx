5c58bce112e77282be0a3e1fb2980951535bbd27
gauravgangwarweb <gauravgangwar501@gmail.com>
2025-01-17 16:54:44 +0530

    Fix typo in Projects component description

--- components/projects/Projects.tsx
+++ components/projects/Projects.tsx
@@ -8,7 +8,7 @@ const Projects = () => {
         Recent Projects
       </h2>
       <p className="my-5 max-w-sm text-sm lg:text-lg text-center mx-auto lg:max-w-xl ">
-        Codekrafters' projects feature innovative designs, intuitive interfaces,
+        Codekrafters projects feature innovative designs, intuitive interfaces,
         and tailored solutions for clients.
       </p>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 px-4 lg:px-0">
