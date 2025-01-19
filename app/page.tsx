5562ac06e371d8d163d79898ba696595064d3bb0
Shriansh Agarwal <41548480+Shriansh2002@users.noreply.github.com>
2025-01-19 10:25:00 +0530

    Remove unused components and assets: deleted FAQ, Pricing, and ProductDemo components along with their related files; removed the favicon image from the project

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
 import { Navbar } from "@/components/navbar";
 import { Hero } from "@/components/hero";
-// import { ProductDemo } from "@/components/product-demo";
 import { Features } from "@/components/features";
 import { Testimonials } from "@/components/testimonials";
-// import { Pricing } from "@/components/pricing";
-// import { FAQ } from "@/components/faq";
 import { Footer } from "@/components/footer";
 import { Team } from "@/components/team";
 import { Projects } from "@/components/projects";
@@ -15,13 +12,10 @@ const Homepage = () => {
 			<Navbar />
 			<main className='mx-auto relative max-w-7xl'>
 				<Hero />
-				{/* <ProductDemo /> */}
 				<Features />
 				<Projects />
 				<Testimonials />
 				<Team />
-				{/* <Pricing /> */}
-				{/* <FAQ /> */}
 			</main>
 			<Footer />
 		</>
