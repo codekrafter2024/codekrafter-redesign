5cb8f8aa386cf2c6e7735fcbf1a14d4a876da609
gauravgangwarweb <gauravgangwar501@gmail.com>
2025-01-19 21:45:08 +0530

    Enhance ProjectCard component to include external links and update project data
    - Added a 'link' prop to the ProjectCard component to enable navigation to external project URLs.
    - Updated project entries in the constants to include corresponding links for "Ihsuktech," "Masterclub," "Onze Bis," and "Gram Circle."

--- components/projects/ProjectCard.tsx
+++ components/projects/ProjectCard.tsx
@@ -1,8 +1,23 @@
+"use client";
+
 import Image from "next/image";
 
-const ProjectCard = ({ name, image, description }: { name: string; image: string; description: string }) => {
+const ProjectCard = ({
+  name,
+  image,
+  description,
+  link,
+}: {
+  name: string;
+  image: string;
+  description: string;
+  link: string;
+}) => {
   return (
-    <div className="cursor-pointer overflow-hidden rounded-xl border border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05] dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15] hover:scale-105 transition-all duration-300">
+    <div
+      onClick={() => link && window.open(link, "_blank")}
+      className="cursor-pointer overflow-hidden rounded-xl border border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05] dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15] hover:scale-105 transition-all duration-300"
+    >
       <Image
         className="w-full h-52 rounded-lg"
         src={image}
@@ -12,9 +27,7 @@ const ProjectCard = ({ name, image, description }: { name: string; image: string
       />
       <div className="text-start p-2">
         <h3 className="text-2xl font-semibold">{name}</h3>
-        <p className="text-sm text-white mt-1">
-          {description}
-        </p>
+        <p className="text-sm text-white mt-1">{description}</p>
       </div>
     </div>
   );
